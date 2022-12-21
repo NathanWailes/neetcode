f56a724091f07bbc30fa0670949fdcb035b9345e
Nathan Wailes <nathan.wailes@gmail.com>
2022-12-21 12:57:33 +0000

    Update gitignore to ignore pyc files, fix a broken test case, and add a success message if all test cases pass.

--- test_runner.py
+++ test_runner.py
@@ -3,57 +3,47 @@ def run_tests():
   solution = Solution()
 
   # First we'll do the test case shown in the task description so that will be the one shown to people first if their code isn't working.  If that passes then we'll do other test cases.
-  test_cases = [
-    {
-      'input': [5, [
-        [0, 1],
-        [1, 2],
-        [3, 4],
-      ]],
-      'output': 2
-    },
-    {
-      'input': [0, []],
-      'output': 0
-    },
-    {
-      'input': [1, []],
-      'output': 1
-    },
-    {
-      'input': [2, []],
-      'output': 2
-    },
-    {
-      'input': [3, []],
-      'output': 3
-    },
-    {
-      'input': [2, [
-        [0, 1],
-      ]],
-      'output': 1
-    },
-    {
-      'input': [3, [
-        [0, 1],
-      ]],
-      'output': 2
-    },
-    {
-      'input': [3, [
-        [1, 2],
-      ]],
-      'output': 2
-    },
-    {
-      'input': [3, [
-        [0, 1],
-        [1, 2],
-      ]],
-      'output': 2
-    }
-  ]
+  test_cases = [{
+    'input': [5, [
+      [0, 1],
+      [1, 2],
+      [3, 4],
+    ]],
+    'output': 2
+  }, {
+    'input': [0, []],
+    'output': 0
+  }, {
+    'input': [1, []],
+    'output': 1
+  }, {
+    'input': [2, []],
+    'output': 2
+  }, {
+    'input': [3, []],
+    'output': 3
+  }, {
+    'input': [2, [
+      [0, 1],
+    ]],
+    'output': 1
+  }, {
+    'input': [3, [
+      [0, 1],
+    ]],
+    'output': 2
+  }, {
+    'input': [3, [
+      [1, 2],
+    ]],
+    'output': 2
+  }, {
+    'input': [3, [
+      [0, 1],
+      [1, 2],
+    ]],
+    'output': 1
+  }]
 
   for test_case in test_cases:
     input = test_case['input']
@@ -63,3 +53,4 @@ def run_tests():
       print('Test case failed.\nInput: {}\nExpected: {}\nGot: {}\n'.format(
         input, expected_output, actual_output))
       return
+  print("All test cases passed!")
