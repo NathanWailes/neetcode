fd09f83a35662fc28209c368f90e01d0d899908b
Nathan Wailes <nathan.wailes@gmail.com>
2023-09-28 03:35:57 +0700

    Switch the project files to a format where I'll have all the Neetcode questions in one repo

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-from typing import List
-
-
-class Solution:
-  def countComponents(self, n: int, edges: List[List[int]]) -> int:
-    pass
-
-
-if __name__ == '__main__':
-  from test_runner import run_tests
-  run_tests()
+from q323 import run_tests
+run_tests()
--- q323.py
+++ q323.py
@@ -1,5 +1,29 @@
+"""
+# Number of Connected Components in an Undirected Graph
+
+You have a graph of `n` nodes.  You are given an integer `n` and an array `edges` where `edges[i] = [aᵢ, bᵢ]` indicates that there is an edge between `aᵢ` and `bᵢ` in the graph.
+
+Return _the number of connected components in the graph_.
+
+**Example 1:**
+
+![Visual representation of the edges in the example](https://i.imgur.com/BCEzWlA.png)
+```
+Input: n = 5, edges = [[0,1],[1,2],[3,4]]
+Output: 2
+```
+"""
+
+from typing import List
+
+
+class Solution:
+
+  def countComponents(self, n: int, edges: List[List[int]]) -> int:
+    pass
+
+
 def run_tests():
-  from main import Solution
   solution = Solution()
 
   # First we'll do the test case shown in the task description so that will be the one shown to people first if their code isn't working.  If that passes then we'll do other test cases.
